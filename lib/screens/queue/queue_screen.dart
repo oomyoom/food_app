3fdf68e2c7ba2894b5f095a8bc597d56ce140dbe
oomyoom <oomyoom@gmail.com>
2023-09-13 11:13:28 +0700

    Update Queue

--- lib/screens/queue/queue_screen.dart
+++ lib/screens/queue/queue_screen.dart
@@ -8,6 +8,12 @@ class QueueScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int totalQueue = 0;
+    for (var item in order) {
+      print('Order ID: ${item.OrderId}');
+      if (item.isCompleted == false) totalQueue++;
+    }
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -23,34 +29,107 @@ class QueueScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(defaultPadding),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: [
-                  Text(
-                    'Number of queues',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.04,
-                  ),
-                  Text(
-                    '0',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                      color: kActiveColor,
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Number of queues',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 36),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04,
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      color: Colors.black26, width: 1))),
+                          child: Column(
+                            children: [
+                              Text(
+                                '$totalQueue',
+                                style: const TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: kActiveColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(defaultPadding),
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black26, width: 1),
+                              color: kMainColor),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Queue ID',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.04,
+                              ),
+                              SingleChildScrollView(
+                                child: Column(
+                                  children: order.asMap().entries.map((entry) {
+                                    final index = entry.key;
+                                    final value = entry.value;
+                                    if (value.isCompleted == false) {
+                                      return Column(
+                                        children: [
+                                          Text(
+                                            'ORD ${value.OrderId}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleMedium!
+                                                .copyWith(color: Colors.white),
+                                          )
+                                        ],
+                                      );
+                                    } else {
+                                      return Container();
+                                    }
+                                  }).toList(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Column(
-                    children: [],
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('AAA')],
+              ),
+            ),
+          )
         ],
       ),
     );
